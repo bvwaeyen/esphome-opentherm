124e35e0b5e20d41d27853d067d321ee3b50cd7b
Bartel Van Waeyenberge <53477925+bvwaeyen@users.noreply.github.com>
2025-05-23 11:51:06 +0200

    Update sensor.py

--- components/opentherm/sensor.py
+++ components/opentherm/sensor.py
@@ -8,13 +8,26 @@ from . import const, schema, validate, generate
 DEPENDENCIES = [ const.OPENTHERM ]
 COMPONENT_TYPE = const.SENSOR
 
+# def get_entity_validation_schema(entity: schema.SensorSchema) -> cv.Schema:
+#     return sensor.sensor_schema(
+#         unit_of_measurement = entity["unit_of_measurement"] if "unit_of_measurement" in entity else sensor.UNDEFINED,
+#         accuracy_decimals = entity["accuracy_decimals"],
+#         device_class=entity["device_class"] if "device_class" in entity else sensor.UNDEFINED,
+#         icon = entity["icon"] if "icon" in entity else sensor.UNDEFINED,
+#         state_class = entity["state_class"]
+#     )
+
 def get_entity_validation_schema(entity: schema.SensorSchema) -> cv.Schema:
     return sensor.sensor_schema(
-        unit_of_measurement = entity["unit_of_measurement"] if "unit_of_measurement" in entity else sensor.UNDEFINED,
-        accuracy_decimals = entity["accuracy_decimals"],
-        device_class=entity["device_class"] if "device_class" in entity else sensor.UNDEFINED,
-        icon = entity["icon"] if "icon" in entity else sensor.UNDEFINED,
-        state_class = entity["state_class"]
+        unit_of_measurement=entity.unit_of_measurement or cv.UNDEFINED,  # pylint: disable=protected-access
+        accuracy_decimals=entity.accuracy_decimals,
+        device_class=entity.device_class or cv.UNDEFINED,  # pylint: disable=protected-access
+        icon=entity.icon or cv.UNDEFINED,  # pylint: disable=protected-access
+        state_class=entity.state_class,
+    ).extend(
+        {
+            cv.Optional(const.CONF_DATA_TYPE): cv.one_of(*MSG_DATA_TYPES),
+        }
     )
 
 CONFIG_SCHEMA = validate.create_component_schema(schema.SENSORS, get_entity_validation_schema)
